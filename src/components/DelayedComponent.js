c25e7c30c713d48b750fd7f4bebe91bb9747bee0
ranjan <ranjan.mylsamy@accenture.com>
2022-10-18 16:20:51 +0530

    testing file

--- src/components/DelayedComponent.js
+++ src/components/DelayedComponent.js
@@ -7,13 +7,7 @@ function DelayedComponent() {
       <p>
         In the context of React.js, concurrency refers to having more than one
         task in progress at once, and concurrent tasks can overlap depending on
-        which is more urgent. For example, while I write this post, I am
-        preparing Jollof rice a Nigerian delicacy. When the time comes to add
-        ingredients to the meal, that will be more urgent, so I/’ll pause
-        writing and attend to that and come back to continue writing when I/’m
-        done. Meanwhile, my food will still be cooking. At different points
-        throughout the writing and cooking processes, my focus will be on what
-        is more urgent.
+        which is more urgent. For example, while I write this post.
       </p>
     </>
   );
